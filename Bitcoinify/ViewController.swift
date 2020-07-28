ce55de3fcfad1bdcbb6909073561a195f1e07d83
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-07-28 17:10:36 +0100

    Initial commit

---
+++ Bitcoinify/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Bitcoinify
+//
+//  Created by Nwachukwu Ejiofor on 28/07/2020.
+//  Copyright © 2020 Nwachukwu Ejiofor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	@IBOutlet weak var bitcoinPriceLabel: UILabel!
+	@IBOutlet weak var currencyPicker: UIPickerView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
